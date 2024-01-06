b5d0f874c541c07419aa2c7fb3c58bce930aae4b
Vladislav <vladyslav.butin@yandex.ru>
2024-01-06 15:28:27 +0300

    First Commit

---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+import '../style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls'
+import { World } from './world';
+import StatsModule from 'three/examples/jsm/libs/stats.module'
+import createUI from './ui';
+
+const stats = new StatsModule();
+document.body.append(stats.dom)
+
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x80a0e0)
+document.body.appendChild(renderer.domElement);
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
+camera.position.set(-32,16,-32);
+
+
+const orbit = new OrbitControls(camera, renderer.domElement)
+orbit.target.set(16,0,16)
+orbit.update()
+
+
+const scene = new THREE.Scene();
+
+const world = new World()
+world.generate()
+scene.add(world)
+
+
+function setupLight() {
+    const light1 = new THREE.DirectionalLight();
+    light1.position.set(1,1,1);
+    scene.add(light1);
+
+    const ambient = new THREE.AmbientLight();
+    ambient.intensity = 0.1
+    scene.add(ambient)
+}
+
+
+
+const animate = () =>{
+    requestAnimationFrame(animate)
+    // cube.rotation.x += 0.01
+    // cube.rotation.y += 0.01
+    stats.update()
+    renderer.render(scene, camera)
+    
+}
+
+setupLight();
+createUI(world);
+animate();
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth,window.innerHeight)
+  })
+
+
+
