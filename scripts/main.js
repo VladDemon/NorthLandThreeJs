f8c1862868780584ecf2417a6cc7637c0c50c28f
Władysław <vladyslav.butin@yandex.ru>
2024-01-16 23:29:05 +0300

    Cm

--- scripts/main.js
+++ scripts/main.js
@@ -5,33 +5,36 @@ import { World } from './world';
 import StatsModule from 'three/examples/jsm/libs/stats.module'
 import createUI from './ui';
 
+
+// stats framerate
 const stats = new StatsModule();
 document.body.append(stats.dom)
 
-
-
+// Rendering
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x80a0e0)
 document.body.appendChild(renderer.domElement);
 
+// Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
 camera.position.set(-32,16,-32);
 
-
+// Orbit
 const orbit = new OrbitControls(camera, renderer.domElement)
 orbit.target.set(16,0,16)
 orbit.update()
 
-
+// Scene
 const scene = new THREE.Scene();
 
+// World Generetation
 const world = new World()
 world.generate()
 scene.add(world)
 
-
+// Light
 function setupLight() {
     const light1 = new THREE.DirectionalLight();
     light1.position.set(1,1,1);
@@ -42,8 +45,7 @@ function setupLight() {
     scene.add(ambient)
 }
 
-
-
+// GameLoopFunc
 const animate = () =>{
     requestAnimationFrame(animate)
     // cube.rotation.x += 0.01
@@ -57,6 +59,7 @@ setupLight();
 createUI(world);
 animate();
 
+// ResizebleWindow
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
